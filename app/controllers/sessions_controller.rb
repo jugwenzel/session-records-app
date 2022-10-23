e86098fdb6660cf0c8a065cd1e6177775aefe558
Juliana Wenzel <jugwenzel@gmail.com>
2022-10-22 22:35:33 -0300

    setting the new session creation (C)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,4 +6,25 @@ class SessionsController < ApplicationController
   def show
     @session = Session.find(params[:id])
   end
+
+  def new
+    @session = Session.new
+  end
+
+  def create
+    @session = Session.new(session_params)
+
+    if @session.save
+      redirect_to @session
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+  def session_params
+    params.require(:session).permit(:title, :date)
+  end
+
 end
