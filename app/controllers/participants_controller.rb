e1367652f487e889b47e6b4c58bcc4e536af5935
Juliana Wenzel <jugwenzel@gmail.com>
2022-10-25 12:45:00 -0300

    Bug fix: params[:session_id]

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,9 +1,10 @@
 class ParticipantsController < ApplicationController
   def create
-    @session = Session.find(params[:article_id])
+    @session = Session.find(params[:session_id])
     @participant = @session.participants.create(participant_params)
     redirect_to session_path(@session)
   end
+
     private
     def participant_params
       params.require(:participant).permit(:name, :role)
