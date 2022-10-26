f5895e3cdacf338b8539ec7b9ead3d385dfe6add
Juliana Wenzel <jugwenzel@gmail.com>
2022-10-26 18:14:08 -0300

    Refacturing concern visible with partials

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,14 +1,9 @@
 class Participant < ApplicationRecord
+  include Visible
+
   belongs_to :session
 
   validates :name, presence: true
   validates :role, presence: true
-
-  VALID_STATUSES = ['public', 'private', 'archived']
-
-  validates :status, inclusion: { in: VALID_STATUSES }
-
-  def archived?
-    status == 'archived'
-  end
+  
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,14 +1,9 @@
 class Session < ApplicationRecord
+  include Visible
+
   has_many :participants
     
   validates :title, presence: true, length: { minimum: 5 }
   validates :date, presence: true
 
-  VALID_STATUSES = ['public', 'private', 'archived']
-
-  validates :status, inclusion: { in: VALID_STATUSES }
-
-  def archived?
-    status == 'archived'
-  end
 end
